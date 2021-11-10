6df75595c26a45ddc0d1d9562c3f610d5a96dfaf
HelenHora <91415890+HelenHora@users.noreply.github.com>
2021-11-10 20:03:59 +0200

    Update CREATE VIEW SEVERAL.sql

--- SQL Managments Studio Scripts/View/CREATE VIEW SEVERAL.sql
+++ SQL Managments Studio Scripts/View/CREATE VIEW SEVERAL.sql
@@ -1,8 +1,12 @@
-CREATE VIEW Several as
-SELECT ID_artwork, Artworks.Title
-FROM Purchases INNER JOIN Artworks
-ON Purchases.ID_Artwork=Artworks.ID
-GROUP BY ID_Artwork, Artworks.Title
-HAVING COUNT (ID_Artwork)>1;
+CREATE VIEW Several
+AS
+SELECT ID_artwork
+	,Artworks.Title
+FROM Purchases
+INNER JOIN Artworks ON Purchases.ID_Artwork = Artworks.ID
+GROUP BY ID_Artwork
+	,Artworks.Title
+HAVING COUNT(ID_Artwork) > 1;
 
-SELECT * FROM [Several];
+SELECT *
+FROM [Several];
