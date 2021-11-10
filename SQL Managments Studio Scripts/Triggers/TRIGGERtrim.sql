2af71949bb87c4f168aa8210a25c7e6a123a844f
HelenHora <91415890+HelenHora@users.noreply.github.com>
2021-11-10 20:06:50 +0200

    Update TRIGGERtrim.sql

--- SQL Managments Studio Scripts/Triggers/TRIGGERtrim.sql
+++ SQL Managments Studio Scripts/Triggers/TRIGGERtrim.sql
@@ -1,10 +1,9 @@
-CREATE TRIGGER TRIMNAMES
-ON Artists
+CREATE TRIGGER TRIMNAMES ON Artists
 FOR INSERT
 AS
 UPDATE Artists
-SET Firstname = LTRIM (RTRIM(inserted.Firstname)),
-	Lastname = LTRIM (RTRIM(inserted.Lastname))
+SET Firstname = LTRIM(RTRIM(inserted.Firstname))
+	,Lastname = LTRIM(RTRIM(inserted.Lastname))
 FROM Artists
-JOIN inserted on Artists.ID=inserted.ID
-WHERE Artists.ID=inserted.ID;
+JOIN inserted ON Artists.ID = inserted.ID
+WHERE Artists.ID = inserted.ID;
