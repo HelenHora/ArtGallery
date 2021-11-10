5adbd44528e12e4c18848a97a823d191ca83f8d7
HelenHora <91415890+HelenHora@users.noreply.github.com>
2021-11-10 20:06:20 +0200

    Update TRIGGER.sql

--- SQL Managments Studio Scripts/Triggers/TRIGGER.sql
+++ SQL Managments Studio Scripts/Triggers/TRIGGER.sql
@@ -1,33 +1,94 @@
-CREATE TRIGGER NameUnique on Artworks
-INSTEAD OF INSERT 
-AS 
-DECLARE @NID_Artist INT, @NTitle VARCHAR(60), @NYearOfWork INT, @NID_Period INT, @NPrice INT
-SELECT @NID_Artist=inserted.ID_Artist, @NTitle=inserted.Title, @NYearOfWork=inserted.YearOfWork, 
-@NID_Period=inserted.ID_Period, @NPrice=inserted.Price From inserted;
-DECLARE @ExistingTitles Table (ArtTitle varchar(60));
-INSERT INTO @ExistingTitles SELECT Title from Artworks;
-DECLARE @ExistingArtists Table (ArtistID INT);
-INSERT INTO @ExistingArtists SELECT ID FROM Artists
+CREATE TRIGGER NameUnique ON Artworks
+INSTEAD OF INSERT
+AS
+DECLARE @NID_Artist INT
+	,@NTitle VARCHAR(60)
+	,@NYearOfWork INT
+	,@NID_Period INT
+	,@NPrice INT
 
-IF @NTitle IN (select * from @ExistingTitles )
-BEGIN 
-  IF  @NID_Artist  IN (select id_artist from Artworks where Title = @NTitle)
-    BEGIN 
-      INSERT INTO Artworks  (ID_Artist, Title, YearOfWork, ID_Period, Price)
-      Values (@NID_Artist, @NTitle, @NYearOfWork, @NID_Period, @NPrice)
-    End
-  ELSE 
-    BEGIN
-      THROW 51000, 'CHECK ARTIST', 1
-    END
-END
-ELSE 
+SELECT @NID_Artist = inserted.ID_Artist
+	,@NTitle = inserted.Title
+	,@NYearOfWork = inserted.YearOfWork
+	,@NID_Period = inserted.ID_Period
+	,@NPrice = inserted.Price
+FROM inserted;
+
+DECLARE @ExistingTitles TABLE (ArtTitle VARCHAR(60));
+
+INSERT INTO @ExistingTitles
+SELECT Title
+FROM Artworks;
+
+DECLARE @ExistingArtists TABLE (ArtistID INT);
+
+INSERT INTO @ExistingArtists
+SELECT ID
+FROM Artists
+
+IF @NTitle IN (
+		SELECT *
+		FROM @ExistingTitles
+		)
 BEGIN
-INSERT INTO Artworks  (ID_Artist, Title, YearOfWork, ID_Period, Price)
-      Values (@NID_Artist, @NTitle, @NYearOfWork, @NID_Period, @NPrice)
+	IF @NID_Artist IN (
+			SELECT id_artist
+			FROM Artworks
+			WHERE Title = @NTitle
+			)
+	BEGIN
+		INSERT INTO Artworks (
+			ID_Artist
+			,Title
+			,YearOfWork
+			,ID_Period
+			,Price
+			)
+		VALUES (
+			@NID_Artist
+			,@NTitle
+			,@NYearOfWork
+			,@NID_Period
+			,@NPrice
+			)
+	END
+	ELSE
+	BEGIN
+		THROW 51000
+			,'CHECK ARTIST'
+			,1
+	END
+END
+ELSE
+BEGIN
+	INSERT INTO Artworks (
+		ID_Artist
+		,Title
+		,YearOfWork
+		,ID_Period
+		,Price
+		)
+	VALUES (
+		@NID_Artist
+		,@NTitle
+		,@NYearOfWork
+		,@NID_Period
+		,@NPrice
+		)
 END;
 
-
-INSERT INTO Artworks  (ID_Artist, Title, YearOfWork, ID_Period, Price)
-VALUES(6, 'The Absinthe Drinker', 1901, 3, 345000);
---VALUES (8, 'Puberty', 1931, 4, 90000);
+INSERT INTO Artworks (
+	ID_Artist
+	,Title
+	,YearOfWork
+	,ID_Period
+	,Price
+	)
+VALUES (
+	6
+	,'The Absinthe Drinker'
+	,1901
+	,3
+	,345000
+	);
+	--VALUES (8, 'Puberty', 1931, 4, 90000);
